7f99e8690270287e9bb8583ceb5215f722ab0e08
Wong <WongM@fnb-corp.com>
2019-05-14 10:53:35 -0400

    added card

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -1,14 +1,27 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { CustomersRoutingModule } from './customers-routing.module';
-import { CustomerListComponent } from './customer-list/customer-list.component';
+import { CustomersRoutingModule } from "./customers-routing.module";
+import { CustomerListComponent } from "./customer-list/customer-list.component";
+import { CustomerCardComponent } from "./customer-card/customer-card.component";
+
+// Material Components
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatIconModule
+} from "@angular/material";
 
 @NgModule({
-  declarations: [CustomerListComponent],
+  declarations: [CustomerListComponent, CustomerCardComponent],
   imports: [
     CommonModule,
-    CustomersRoutingModule
+    CustomersRoutingModule,
+
+    // Material Components
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule
   ]
 })
-export class CustomersModule { }
+export class CustomersModule {}
--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { OrdersRoutingModule } from './orders-routing.module';
-import { OrderListComponent } from './order-list/order-list.component';
+import { OrdersRoutingModule } from "./orders-routing.module";
+import { OrderListComponent } from "./order-list/order-list.component";
+
+// Material components
+import { MatTabsModule } from "@angular/material";
 
 @NgModule({
   declarations: [OrderListComponent],
-  imports: [
-    CommonModule,
-    OrdersRoutingModule
-  ]
+  imports: [CommonModule, OrdersRoutingModule, MatTabsModule]
 })
-export class OrdersModule { }
+export class OrdersModule {}
